46c5084d89e49324dfcf255b0d3a1f7ac680d913
inacio <40220299@esmad.ipp.pt>
2023-11-22 14:52:40 +0000

    home page create

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,302 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Mercado Poupança',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AppAdvertsBar extends StatefulWidget {
+  const AppAdvertsBar({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  @override
+  State<AppAdvertsBar> createState() => _AppAdvertsBar();
+}
+
+class AppSeachBar extends StatefulWidget {
+  const AppSeachBar({super.key});
+
+  @override
+  State<AppSeachBar> createState() => _AppSeachBar();
+}
+
+class AppBuilderContainer extends StatefulWidget {
+  const AppBuilderContainer({super.key});
+
+  @override
+  State<AppBuilderContainer> createState() => _AppBuilderContainer();
+}
+
+class HamburgerMenu extends StatefulWidget {
+  const HamburgerMenu({super.key});
+
+  @override
+  State<HamburgerMenu> createState() => _HamburgerMenu();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return  const Scaffold(
+      body: SafeArea(
+        top: false,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                AppAdvertsBar(),
+                AppSeachBar(),
+                Expanded(
+                  child: AppBuilderContainer()
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Positioned(left: 0, bottom: 0, child: HamburgerMenu()),
           ],
-        ),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _AppAdvertsBar extends State<AppAdvertsBar> {
+   String img = 'lib/img/NovoProjeto.png';
+  int i = 0;
+  bool color = true;
+  bool imgOpacity = true;
+
+  @override
+  void initState () {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    super.initState();
+
+    Timer.periodic(const Duration(seconds: 10), (timer) {
+      setState(() {
+        if(i == 0){
+          imgOpacity = !imgOpacity;
+          color = !color;
+          i = i + 1;
+        } else {
+          imgOpacity = !imgOpacity;
+          color = !color;
+          i = i - 1;
+        }
+      });
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+    return Row(
+        children: [
+          AnimatedContainer(
+            onEnd: () {
+              setState(() {
+                if(i == 0){
+                  img = 'lib/img/NovoProjeto.png';
+                } else {
+                  img = 'lib/img/sddefault.jpg';
+                }
+                imgOpacity = !imgOpacity;
+              });
+            },
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeOut,
+            color: color ? const Color(0xFF508932): Colors.black,
+            width: screenWidth,
+            height: screenheight * 0.07,
+            child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeOut,
+                  opacity:  imgOpacity ? 1.0 : 0.0,
+                  child:  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.asset(
+                    img,
+                    width: 150,
+                    height: 50,
+                ),
+                 Padding(
+                  padding: EdgeInsets.only(right: screenWidth * 0.05),
+                  child: Text(
+                  'Saiba mais',
+                  style: TextStyle(
+                    color: Colors.white, 
+                    fontSize: (screenheight / screenWidth) * 8,
+                  ),
+                ),
+                ),
+              ],
+            ), 
+            ),
+          ),
+        ],
+    );
+  }
+}
+
+class _AppSeachBar extends State<AppSeachBar>{
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      top: false,
+      child: Container(
+        height: screenheight * 0.09,
+        width: screenWidth,
+        color: Colors.grey,
+      ),
+    );
+  }
+}
+
+class _AppBuilderContainer extends State<AppBuilderContainer>{
+
+  @override
+  Widget build(BuildContext context) {
+
+    // ignore: avoid_unnecessary_containers
+    return ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return const ListTile(
+            title: Text('a'),
+          );
+        },
+      );
+  }
+
+}
+
+class _HamburgerMenu extends State<HamburgerMenu> {
+  bool hamburgerOn = false;
+  bool iconsHamburgerOn = true;
+
+  @override
+  Widget build(BuildContext context){
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 5, 5),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                if(hamburgerOn == false){
+                  iconsHamburgerOn = !iconsHamburgerOn;
+                }
+              });
+
+              Timer(const Duration(milliseconds: 100), () {
+                setState(() {
+                  if(hamburgerOn == false){
+                    hamburgerOn = !hamburgerOn;
+                  }
+                });
+              });
+            },
+            onHorizontalDragEnd:(details) {
+              setState(() {
+                if(hamburgerOn == true){
+                  iconsHamburgerOn = !iconsHamburgerOn;
+                }
+              });
+
+              Timer(const Duration(milliseconds: 100), () {
+                setState(() {
+                  if(hamburgerOn == true){
+                    hamburgerOn = !hamburgerOn;
+                  }
+                });
+              });
+            },
+            child: AnimatedContainer(
+              onEnd: () {
+                setState(() {
+                  iconsHamburgerOn = !iconsHamburgerOn;
+                });
+              },
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeOut,
+              height: screenheight * 0.1,
+              width: hamburgerOn ? screenWidth * 0.95 : screenWidth * 0.22,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                border: Border.all(
+                  color: Colors.yellow,
+                ),
+                borderRadius: const BorderRadius.all(Radius.circular(50))
+              ),
+              child: SafeArea(
+                top: false,
+                  child: Visibility(
+                  visible: hamburgerOn,
+                  replacement: AnimatedOpacity(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeOut,
+                    opacity: iconsHamburgerOn ? 1 : 0,
+                    child: const Icon(
+                      Icons.list,
+                      color: Colors.white,
+                    )
+                  ),
+                  child: AnimatedOpacity(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeOut,
+                    opacity: iconsHamburgerOn ? 1 : 0,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.list,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.shopping_bag,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.account_circle,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.settings,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
